c58c4270a2ad57a994d54146cc3e8d6588c5bff8
YALLAULLA TEJASWI <yteja2912@gmail.com>
2016-10-04 23:15:42 +0530

    multipart

--- src/main/java/com/niit/shoppingcart1/controllers/ProductController.java
+++ src/main/java/com/niit/shoppingcart1/controllers/ProductController.java
@@ -1,5 +1,14 @@
 package com.niit.shoppingcart1.controllers;
 
+
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,10 +17,13 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.niit.shoppingcart1.DAO.CategoryDAO;
 import com.niit.shoppingcart1.DAO.ProductDAO;
+import com.niit.shoppingcart1.DAO.SupplierDAO;
 import com.niit.shoppingcart1.modal.Product;
 
 @Controller
@@ -19,14 +31,24 @@ public class ProductController {
 
 
 		@Autowired
-	 ProductDAO productDAO;
+	 private ProductDAO productDAO;
+		
+		@Autowired
+		private CategoryDAO categoryDAO;
+		
+		@Autowired
+		private SupplierDAO supplierDAO;
+		
+		private Path path;
 		
 		@RequestMapping(value="/Product",method = RequestMethod.GET)
-		public ModelAndView landPage(@ModelAttribute("product")Product product,BindingResult result,Model model)
+		public ModelAndView productlandPage(@ModelAttribute("product")Product product,BindingResult result,Model model)
 		{
-			ModelAndView mv=new ModelAndView("/admin");
+			ModelAndView mv=new ModelAndView("/Product");
 			mv.addObject("userclickedproduct", "true");
-			mv.addObject("allData",productDAO.list());
+			mv.addObject("allproduct",productDAO.list());
+			mv.addObject("allcategory",categoryDAO.list());
+			mv.addObject("allsupplier",supplierDAO.list());
 			return mv;
 			
 		}
@@ -38,18 +60,43 @@ public class ProductController {
 			model.addAttribute("allData",productDAO.list());
 			return "Product";
 		}*/
-	@RequestMapping(value="/addproduct")
-	public String addItem(@ModelAttribute("product") Product p){
+
+		@RequestMapping(value="/addProduct",method = RequestMethod.POST)
+		public String addProduct(@ModelAttribute("product") Product product,RedirectAttributes redirectAttributes, Model model,HttpServletRequest request, MultipartFile file) throws IOException
+		{
+			//redirectAttributes.addAttribute("product", product);
+			 this.productDAO.saveOrUpdate(product);
+		    		System.out.println("Data Inserted");
+		      		MultipartFile image = product.getImage();
+		            String root = request.getSession().getServletContext().getRealPath("/");
+		            path = Paths.get(root+"\\resources\\images\\product\\"+product.getId()+".jpg");
+		            System.out.println("Path="+path);
+		            System.out.println("File name = " + product.getImage().getOriginalFilename());
+		          
+		           if(image!=null && !image.isEmpty()){
+		            
+		            	try
+		            	{
+		            		image.transferTo(new File(path.toString()));
+		            		System.out.println("Image saved  in:"+path.toString());
+		            	}
+		            	catch(Exception e)
+		            	{
+		            		e.printStackTrace();
+		            		System.out.println("Image not saved");
+		            	}
+		           }
+		    	
+		            return "redirect:/Product";
+					
 			
-			productDAO.saveOrUpdate(p);
-			return "redirect:/Product";
-			
-		}
+}
 		@RequestMapping(value="/ItemByproduct/{id}")
 		public String editItem(@PathVariable("id") int id,RedirectAttributes redirectAttributes)
 		{
 			redirectAttributes.addFlashAttribute("product", productDAO.get(id));
 			return "redirect:/Product";
+			
 			
 	}
 		@RequestMapping(value="/deleteproduct/{id}")
--- src/main/java/com/niit/shoppingcart1/modal/Product.java
+++ src/main/java/com/niit/shoppingcart1/modal/Product.java
@@ -4,8 +4,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
 
 @Entity
 @Table(name="PRODUCT")
@@ -18,29 +20,82 @@ public class Product {
 private String name;
 private String description;
 private double  price;
- public int getId() {
+
+private int category_id;
+private int supplier_id;
+
+
+@Transient
+private MultipartFile image;
+
+
+public int getId() {
 	return id;
 }
-public String getName() {
-	return name;
-}
-public String getDescription() {
-	return description;
-}
-public double getPrice() {
-	return price;
-}
+
+
 public void setId(int id) {
 	this.id = id;
 }
+
+
+public String getName() {
+	return name;
+}
+
+
 public void setName(String name) {
 	this.name = name;
 }
+
+
+public String getDescription() {
+	return description;
+}
+
+
 public void setDescription(String description) {
 	this.description = description;
 }
-public void setPrice(double price) {
-	this.price = price;
-} 
+
+
+public double getPrice() {
+	return price;
 }
 
+
+public void setPrice(double price) {
+	this.price = price;
+}
+
+
+public int getCategory_id() {
+	return category_id;
+}
+
+
+public void setCategory_id(int category_id) {
+	this.category_id = category_id;
+}
+
+
+public int getSupplier_id() {
+	return supplier_id;
+}
+
+
+public void setSupplier_id(int supplier_id) {
+	this.supplier_id = supplier_id;
+}
+
+
+public MultipartFile getImage() {
+	return image;
+}
+
+
+public void setImage(MultipartFile image) {
+	this.image = image;
+}
+
+}
